0784f4c4d663c2d93b68b969a3fe6c9b8eb195f2
hophantennhan <67749506+hophantennhan@users.noreply.github.com>
2020-11-12 10:56:14 +0700

    Update cuối buổi 12/11

--- Staff/app/src/main/java/com/example/staff/KhuVuc.java
+++ Staff/app/src/main/java/com/example/staff/KhuVuc.java
@@ -2,13 +2,26 @@ package com.example.staff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class KhuVuc extends AppCompatActivity {
-
+    ImageView imgPhongLanh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_khu_vuc);
+        anXa();
+        imgPhongLanh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(KhuVuc.this,PhongLanh.class));
+            }
+        });
+    }
+    private void anXa(){
+        imgPhongLanh = findViewById(R.id.imgPhongLanh);
     }
 }
--- Staff/app/src/main/java/com/example/staff/PhongLanh.java
+++ Staff/app/src/main/java/com/example/staff/PhongLanh.java
@@ -2,13 +2,27 @@ package com.example.staff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class PhongLanh extends AppCompatActivity {
-
+Button btnBan1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phong_lanh);
+anXa();
+btnBan1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(PhongLanh.this,ChiTietBan.class));
+    }
+});
+    }
+    private void anXa(){
+        btnBan1 = findViewById(R.id.btnBan1);
     }
 }
