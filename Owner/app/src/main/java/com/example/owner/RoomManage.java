352aa3cc2c774428c79192cd65880a57d377771c
Huỳnh Duy Quốc <nhoxkenl090@gmail.com>
2020-11-12 07:28:30 +0700

    Dev_Quoc
    1st
    Area_Manager
    Drawer_menu

---
+++ Owner/app/src/main/java/com/example/owner/RoomManage.java
@@ -0,0 +1,14 @@
+package com.example.owner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomManage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_manage);
+    }
+}
