fa5975a65aedc97eb08f5711b832fe01ca91a86a
dangtuanphong238 <dangtuanphong238@github.com>
2020-11-12 09:24:14 +0700

    Commit update navigate screen SplashScreen ->  Login -> Main

--- Owner/app/src/main/java/com/example/owner/AreaManage.java
+++ Owner/app/src/main/java/com/example/owner/AreaManage.java
@@ -3,12 +3,22 @@ package com.example.owner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class AreaManage extends AppCompatActivity {
-
+    ImageButton btnMnu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area_manage);
+        anhXa();
+        setOnClick();
+    }
+    private void setOnClick(){
+
+    }
+    private void anhXa(){
+        btnMnu = findViewById(R.id.btnMnu);
     }
 }
---
+++ Owner/app/src/main/java/com/example/owner/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.owner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 2500;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
