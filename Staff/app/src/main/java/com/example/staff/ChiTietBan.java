9737d15b9d16c8daa3263d223778369b5f744948
hophantennhan <67749506+hophantennhan@users.noreply.github.com>
2020-11-13 07:08:26 +0700

    Commit đầu ngày 13/11/2020

--- Staff/app/src/main/java/com/example/staff/ChiTietBan.java
+++ Staff/app/src/main/java/com/example/staff/ChiTietBan.java
@@ -3,12 +3,28 @@ package com.example.staff;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ChiTietBan extends AppCompatActivity {
-
+ListView lvChiTietBan;
+ArrayList<String> arrayCourse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chi_tiet_ban);
+        anXa();
+        arrayCourse = new ArrayList<>();
+        arrayCourse.add("Bạc xỉu");
+        arrayCourse.add("Đen đá");
+        arrayCourse.add("Phin đá");
+        ArrayAdapter adapter = new ArrayAdapter(
+                ChiTietBan.this, android.R.layout.simple_list_item_1,arrayCourse);
+lvChiTietBan.setAdapter(adapter);
+    }
+    private void anXa(){
+        lvChiTietBan = findViewById(R.id.lvChiTietBan);
     }
 }
---
+++ Staff/app/src/main/java/com/example/staff/DatMon.java
@@ -0,0 +1,14 @@
+package com.example.staff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatMon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dat_mon);
+    }
+}
