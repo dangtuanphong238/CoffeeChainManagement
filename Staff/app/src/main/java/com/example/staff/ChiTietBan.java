e09f40d7df43d38c1fe95b0b55611c02f9c3dea3
hophantennhan <67749506+hophantennhan@users.noreply.github.com>
2020-11-14 07:24:30 +0700

    Update đầu ngày 14/11

--- Staff/app/src/main/java/com/example/staff/ChiTietBan.java
+++ Staff/app/src/main/java/com/example/staff/ChiTietBan.java
@@ -1,7 +1,9 @@
 package com.example.staff;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,39 +14,29 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class ChiTietBan extends AppCompatActivity {
-ListView lvChiTietBan;
-ArrayList<String> arrayCourse;
-ArrayAdapter adapter;
+    RecyclerView rcChiTiet,rcChiTiet1;
+    ArrayList<MonAn> listMonAn;
+    MonAnAdapter monAnAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chi_tiet_ban);
-        anXa();
-        arrayCourse = new ArrayList<>();
-        arrayCourse.add("Bạc xỉu");
-        arrayCourse.add("Đen đá");
-        arrayCourse.add("Phin đá");
-        arrayCourse.add("Bạc xỉu");
-        arrayCourse.add("Đen đá");
-        arrayCourse.add("Phin đá");
-        arrayCourse.add("Bạc xỉu");
-        arrayCourse.add("Đen đá");
-        arrayCourse.add("Phin đá");
-        arrayCourse.add("Bạc xỉu");
-        arrayCourse.add("Đen đá");
-        arrayCourse.add("Phin đá");
+        rcChiTiet = findViewById(R.id.rcChitiet);
+        listMonAn = new ArrayList<>();
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        listMonAn.add(new MonAn("Bacxiu", 200000, R.drawable.cf));
+        monAnAdapter = new MonAnAdapter(getApplicationContext(), listMonAn);
+        rcChiTiet.setAdapter(monAnAdapter);
+    }
 
-        ArrayAdapter adapter = new ArrayAdapter(ChiTietBan.this, android.R.layout.simple_list_item_1,arrayCourse);
-lvChiTietBan.setAdapter(adapter);
-lvChiTietBan.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(ChiTietBan.this,DatMon.class);
-        startActivity(intent);
-    }
-});
-    }
-    private void anXa(){
-        lvChiTietBan = findViewById(R.id.lvChiTietBan);
-    }
 }
