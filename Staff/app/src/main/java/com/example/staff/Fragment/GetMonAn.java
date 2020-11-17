1a19d5f6064adaa327a80adbb9dfc20a49a86fa3
hophantennhan <67749506+hophantennhan@users.noreply.github.com>
2020-11-17 20:57:42 +0700

    Commit

---
+++ Staff/app/src/main/java/com/example/staff/Fragment/GetMonAn.java
@@ -0,0 +1,27 @@
+package com.example.staff.Fragment;
+
+public class GetMonAn {
+    String gia;
+    String ten;
+
+    public GetMonAn(String gia, String ten) {
+        this.gia = gia;
+        this.ten = ten;
+    }
+
+    public String getGia() {
+        return gia;
+    }
+
+    public void setGia(String gia) {
+        this.gia = gia;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+}
--- Staff/app/src/main/java/com/example/staff/LoginScreen.java
+++ Staff/app/src/main/java/com/example/staff/LoginScreen.java
@@ -37,39 +37,7 @@ public class LoginScreen extends AppCompatActivity {
         password = findViewById(R.id.edtPassWord);
         idcafe = findViewById(R.id.edtIdCafe);
         database = FirebaseDatabase.getInstance();
-        myRef2 = database.getReference().child("OwnerManager");
-        myRef2.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot dataSnapshot:snapshot.getChildren()){
-                    lstOwnerList.add(dataSnapshot.getKey());
-                    System.out.println("" + lstOwnerList.toString());
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-//        myRef = database.getReference().child("OwnerManager").child(maCh);
-//        myRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-//                    User user = dataSnapshot.getValue(User.class);
-//                    System.out.println(user.user + " " + user.pass + " ");
-//                    System.out.println("abcDemo1"+ lstUsers.toString());
-//                    lstUsers.add(user);
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
+        getListOwner();
         setOnClick();
     }
     public void getId(){
@@ -83,33 +51,8 @@ public class LoginScreen extends AppCompatActivity {
                 boolean isSuccess = false;
                 for(String owner:lstOwnerList){
                     if(owner.equals(idcafe.getEditText().getText().toString())){
-//                        maCh = idcafe.getEditText().getText().toString();
                         maCh = owner;
                         Toast.makeText(LoginScreen.this, maCh, Toast.LENGTH_SHORT).show();
-                        myRef = database.getReference().child("OwnerManager").child(maCh);
-                        myRef.addValueEventListener(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                                    User user = dataSnapshot.getValue(User.class);
-                                    System.out.println(user.user + " " + user.pass + " ");
-                                    System.out.println("abcDemo1"+ lstUsers.toString());
-                                    lstUsers.add(user);
-                                    System.out.println("lstUser " + lstUsers.toString());
-                                    if(username.equals(user.user)&&password.equals(user.pass)){
-                                        System.out.println("success");
-                                    }
-                                    else {
-                                        System.out.println("failed");
-                                    }
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-
-                            }
-                        });
                         if(owner.equals(idcafe.getEditText().getText().toString())) {
                         for (User user : lstUsers) {
                             if (user.user.equals(username.getEditText().getText().toString()) && user.pass.equals(password.getEditText().getText().toString())) {
@@ -133,6 +76,24 @@ public class LoginScreen extends AppCompatActivity {
 
             }
 
+        });
+    }
+    private void getListOwner(){
+        database = FirebaseDatabase.getInstance();
+        myRef2 = database.getReference().child("OwnerManager");
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    lstOwnerList.add(dataSnapshot.getKey());
+                    System.out.println("" + lstOwnerList.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
     }
 }
