57098b84bb6c6507971a4044048c7a6cf9d26508
hophantennhan <67749506+hophantennhan@users.noreply.github.com>
2020-11-20 13:07:36 +0700

    Update 13h07 20/11 Hoàn thành show danh sách bàn ra gridview

--- Staff/app/src/main/java/com/example/staff/PhongLanh.java
+++ Staff/app/src/main/java/com/example/staff/PhongLanh.java
@@ -1,28 +1,72 @@
 package com.example.staff;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import com.example.staff.Fragment.BanAdapter;
 
 public class PhongLanh extends AppCompatActivity {
-Button btnBan1;
+    Button btnBan1;
+    GridView gridView;
+    Ban ban = new Ban();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phong_lanh);
-anXa();
-btnBan1.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        startActivity(new Intent(PhongLanh.this,ChiTietBan.class));
+        gridView = findViewById(R.id.gridviewPl);
+
+
+        String[] values = {
+                "Ban 1",
+                "Ban 2",
+                "Ban 3",
+                "Ban 4",
+                "Ban 5",
+                "Ban 6",
+                "Ban 7",
+                "Ban 8",
+                "Ban 9",
+                "Ban 10",
+                "Ban 11",
+                "Ban 12",
+                "Ban 13",
+                "Ban 14",
+                "Ban 15",
+        };
+        int [] images = {
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table,
+                R.drawable.ic_table
+        };
+
+        BanAdapter banAdapter = new BanAdapter(this,values,images);
+        gridView.setAdapter(banAdapter);
+//anXa();
+//btnBan1.setOnClickListener(new View.OnClickListener() {
+//    @Override
+//    public void onClick(View v) {
+//        startActivity(new Intent(PhongLanh.this,ChiTietBan.class));
+//    }
+//});
+//    }
+//    private void anXa(){
+//        btnBan1 = findViewById(R.id.btnBan1);
+//    }
     }
-});
-    }
-    private void anXa(){
-        btnBan1 = findViewById(R.id.btnBan1);
-    }
+
 }
