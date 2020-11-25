7717e162cd7f731426f91d6f02c7bd2e1b641dcf
hophantennhan <67749506+hophantennhan@users.noreply.github.com>
2020-11-25 19:06:11 +0700

    Update 19h05 25/11/2020 Hoàn thành setdata cho 2 màn hình khu vực và màn hình phòng

---
+++ Staff/app/src/main/java/com/example/staff/KhuVucMD.java
@@ -0,0 +1,14 @@
+package com.example.staff;
+
+public class KhuVucMD {
+    public String soban;
+    public String ten;
+
+    public KhuVucMD(String soban, String ten) {
+        this.soban = soban;
+        this.ten = ten;
+    }
+
+    public KhuVucMD() {
+    }
+}
---
+++ Staff/app/src/main/java/com/example/staff/PhongMd.java
@@ -0,0 +1,30 @@
+package com.example.staff;
+
+public class PhongMd {
+    private String ten;
+    private String soban;
+
+    public PhongMd(String ten, String soban) {
+        this.ten = ten;
+        this.soban = soban;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getSoban() {
+        return soban;
+    }
+
+    public void setSoban(String soban) {
+        this.soban = soban;
+    }
+
+    public PhongMd() {
+    }
+}
