f78bd3a711bda1edf38cfcba270929406a831d70
hophantennhan <67749506+hophantennhan@users.noreply.github.com>
2020-12-10 07:22:09 +0700

    Update 7h21p 10/12/2020

--- Staff/app/src/main/java/com/example/staff/Adapter/MessageAdapter.java
+++ Staff/app/src/main/java/com/example/staff/Adapter/MessageAdapter.java
@@ -13,41 +13,38 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.staff.Message;
 import com.example.staff.R;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder>{
 
     private ArrayList<Message> listData;
-//    private String myID, idFromDB;
+    private String myID, idFromDB;
+    private int pos;
+    public MessageAdapter(ArrayList<Message> listData, String myID) {
+        this.listData = listData;
+        this.myID = myID;
+    }
     public MessageAdapter(ArrayList<Message> listData) {
         this.listData = listData;
-//        this.myID = myID;
-//        this.idFromDB = idFromDB;
-    }
 
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.cus_list_chat_room,parent,false);
-        return new ViewHolder(view);
+        View view1 = LayoutInflater.from(parent.getContext()).inflate(R.layout.cus_list_chat_room_right,parent,false);
+        System.out.println("view type " + pos);
+        if(myID.equals(listData.get(pos).getUserID()))
+        {
+            return new ViewHolder(view);
+
+        }else {
+            return new ViewHolder(view1);
+        }
     }
 
-//    @NonNull
-//    @Override
-//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-////        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.cus_list_chat_room,parent,false);
-////        return new ViewHolder(view);
-//        if(!myID.equals(idFromDB))
-//        {
-//            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.cus_list_chat_room,parent,false);
-//            return new ViewHolder(view);
-//        }
-//        else{
-//            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.cus_list_chat_room_second,parent,false);
-//            return new ViewHolder(view);
-//        }
-//    }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
@@ -61,6 +58,13 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
     @Override
     public int getItemCount() {
         return listData.size();
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        pos = position;
+        return super.getItemViewType(position);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
