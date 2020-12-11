89beefb1a7768b5c7f819736cac7bdbcc7b3a23e
dangtuanphong238 <dangtuanphong238@github.com>
2020-12-11 09:03:22 +0700

    Commit add area + table (no done)

---
+++ Founder/app/src/main/java/com/example/founder/Interfaces/GetValueTable.java
@@ -0,0 +1,8 @@
+package com.example.founder.Interfaces;
+
+import java.util.ArrayList;
+
+public interface GetValueTable {
+    void GetValue(String value);
+    void returnValueForAddTableActivity(ArrayList arrayList);
+}
