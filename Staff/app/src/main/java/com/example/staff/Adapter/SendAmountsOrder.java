c53bec77376272be6690beb1440c6e75cca1fe96
hophantennhan <67749506+hophantennhan@users.noreply.github.com>
2020-12-31 10:26:52 +0700

    Commit Up

--- Staff/app/src/main/java/com/example/staff/Adapter/SendAmountsOrder.java
+++ Staff/app/src/main/java/com/example/staff/Adapter/SendAmountsOrder.java
@@ -1,7 +1,10 @@
 package com.example.staff.Adapter;
 
 import com.example.staff.Model.MealModel;
+import com.example.staff.Model.ModelCombo;
 
 public interface SendAmountsOrder {
     void sendAmount(int times, MealModel meal, int last_amounts);
+
+    void sendAmount(int times, ModelCombo mealmodel,int last_amounts);
 }
