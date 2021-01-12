b3aa8da06cca78fe4844fda8f0e47a30349c1d15
hophantennhan <67749506+hophantennhan@users.noreply.github.com>
2021-01-12 20:35:40 +0700

    cmit

--- Staff/app/src/main/java/com/example/staff/Model/MealModel.java
+++ Staff/app/src/main/java/com/example/staff/Model/MealModel.java
@@ -1,25 +1,81 @@
 package com.example.staff.Model;
 
-import android.graphics.Bitmap;
-
-import java.io.Serializable;
-
 public class MealModel {
     String meal_category;
     String meal_id;
     String meal_price;
     String meal_name;
     String meal_image;
+    String meal_description;
+    String meal_price_total;
+    String meal_uu_dai;
+
+    public int getAmounts() {
+        return amounts;
+    }
+
+    public void setAmounts(int amounts) {
+        this.amounts = amounts;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    //Tempt set offline
+    int amounts = 0;
+    int price = 0;
+    Boolean flag = false;
 
     public MealModel() {
     }
 
-    public MealModel(String meal_category, String meal_id, String meal_price, String meal_name, String meal_image) {
+    public MealModel(ModelCombo mealModel) {
+        meal_category = mealModel.meal_category;
+        meal_id = mealModel.meal_id;
+        meal_price = mealModel.meal_price;
+        meal_name = mealModel.meal_name;
+        meal_image = mealModel.meal_image ;
+        meal_description = mealModel.meal_description;
+        meal_price_total = mealModel.meal_price_total;
+        meal_uu_dai = mealModel.meal_uu_dai;
+    }
+
+    public MealModel(String meal_category, String meal_id, String meal_price, String meal_name, String meal_image, String meal_description, String meal_price_total, String meal_uu_dai) {
         this.meal_category = meal_category;
         this.meal_id = meal_id;
         this.meal_price = meal_price;
         this.meal_name = meal_name;
         this.meal_image = meal_image;
+        this.meal_description = meal_description;
+        this.meal_price_total = meal_price_total;
+        this.meal_uu_dai = meal_uu_dai;
+    }
+
+    public MealModel(String meal_category, String meal_id, String meal_price, String meal_name, String meal_image, String meal_description, String meal_price_total, String meal_uu_dai, int amounts, int price, boolean flag) {
+        this.meal_category = meal_category;
+        this.meal_id = meal_id;
+        this.meal_price = meal_price;
+        this.meal_name = meal_name;
+        this.meal_image = meal_image;
+        this.meal_description = meal_description;
+        this.meal_price_total = meal_price_total;
+        this.meal_uu_dai = meal_uu_dai;
+        this.amounts = amounts;
+        this.price = price;
+        this.flag = flag;
+    }
+
+    public Boolean getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Boolean flag) {
+        this.flag = flag;
     }
 
     public int getID() {
@@ -67,6 +123,30 @@ public class MealModel {
         this.meal_image = meal_image;
     }
 
+    public String getMeal_description() {
+        return meal_description;
+    }
+
+    public void setMeal_description(String meal_description) {
+        this.meal_description = meal_description;
+    }
+
+    public String getMeal_price_total() {
+        return meal_price_total;
+    }
+
+    public void setMeal_price_total(String meal_price_total) {
+        this.meal_price_total = meal_price_total;
+    }
+
+    public String getMeal_uu_dai() {
+        return meal_uu_dai;
+    }
+
+    public void setMeal_uu_dai(String meal_uu_dai) {
+        this.meal_uu_dai = meal_uu_dai;
+    }
+
     @Override
     public String toString() {
         return "MealModel{" +
@@ -75,6 +155,9 @@ public class MealModel {
                 ", meal_price='" + meal_price + '\'' +
                 ", meal_name='" + meal_name + '\'' +
                 ", meal_image='" + meal_image + '\'' +
+                ", meal_description='" + meal_description + '\'' +
+                ", meal_price_total='" + meal_price_total + '\'' +
+                ", meal_uu_dai='" + meal_uu_dai + '\'' +
                 '}';
     }
 }
